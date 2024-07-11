8d02afc517f7609ba1f3f081271ef36a3992ae70
Andy Huang <cerberus@ayxh.dev>
2024-07-11 12:36:44 +0000

    First commit

---
+++ src/main/java/com/corndel/nozama/Db.java
@@ -0,0 +1,13 @@
+package com.corndel.nozama;
+
+import java.sql.Connection;
+
+import java.util.Properties;
+
+public class Db {
+  final String dbUrl = "jdbc:sqlite:nozama.db";
+
+  public static Connection getConnection() {
+
+  }
+}
