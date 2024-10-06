6eed534cffd93711f2ead392a5c4dac413938340
Simon Haines <nimon@hotmail.co.uk>
2024-10-06 21:32:01 +0100

    wording

--- src/main/java/com/corndel/nozama/exercises/D3E1.java
+++ src/main/java/com/corndel/nozama/exercises/D3E1.java
@@ -3,19 +3,14 @@ package com.corndel.nozama.exercises;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
-// TODO: https://tech-docs.corndel.com/javalin/routing.html
-
 public class D3E1 {
-  // This is the counter instance you should be using for this exercise:
+  // This is our counter:
   public static Counter counter = new Counter();
 
   /**
-   * Creates a Javalin app with two endpoints:
-   * - GET /counter responds with the current count in the response body
-   * as a JSON object, i.e. { "count": <current count> }.
-   * - PUT /counter/increment increases the counter by 1 and then responds
-   * with the count.
+   * Creates a Javalin app with two endpoints.
    *
+   * @see https://tech-docs.corndel.com/javalin/routing.html
    * @return a configured Javalin instance
    */
   public static Javalin createApp() {
@@ -26,15 +21,27 @@ public class D3E1 {
   }
 }
 
+class CounterController {
+  /**
+   * Responds with the current counter as a JSON object, e.g. { "count": 3 }.
+   */
+  public static void getCounter(Context ctx) {
+    // TODO
+  }
+
+  /**
+   * Increases the counter by 1 and then responds with the count.
+   */
+  public static void increment(Context ctx) {
+    // TODO
+  }
+}
+
 class Counter {
   private int count;
 
   public Counter() {
     count = 0;
-  }
-
-  public void increment() {
-    count++;
   }
 
   public int getCount() {
@@ -43,26 +50,5 @@ class Counter {
 
   public void setCount(int count) {
     this.count = count;
-  }
-}
-
-class CounterController {
-
-  /**
-   * A GET /counter endpoint that returns the current count in the response body
-   * as a JSON object, i.e. { "count": <current count> }.
-   */
-  public static void getCounter(Context ctx) {
-    // TODO
-  }
-
-  /**
-   * Increases the counter by 1 and then responds with the count.
-   *
-   * <p>
-   * Example response: { "count": 1 }
-   */
-  public static void increment(Context ctx) {
-    // TODO
   }
 }
